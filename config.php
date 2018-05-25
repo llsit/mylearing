7459a63c4425bbbaa054f27139c4246a00b57d42
llsit <thenuts53@gmail.com>
2018-05-25 14:48:40 +0700

    fix imporve

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-    define('DB_SERVER', '128.199.149.14');
+    define('DB_SERVER', 'localhost');
     define('DB_USERNAME', 'thenut');
     define('DB_PASSWORD', 'JPXnLhjOzKjqWAuM');
     define('DB_DATABASE', 'mylearning')
